853c548d519e42768595ab6cabc63106c7f4bf3d
weldon <weldon502@163.com>
2021-06-05 21:37:32 +0800

    init

---
+++ src/main/java/com/example/wlh_develop/WlhDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.example.wlh_develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WlhDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WlhDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wlh_develop/WlhDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wlh_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WlhDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
