7866779ec8b5fb0b979d78e08002edb71908f8a3
weldon <weldon502@163.com>
2021-06-07 23:48:47 +0800

    引入首页，热部署，layui，thymeleaf

---
+++ src/main/java/com/example/wlh_develop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.wlh_develop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
