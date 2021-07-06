55339c8ce5af6d40113c357c0e79a49d57dea185
weldon <weldon502@163.com>
2021-07-07 00:03:16 +0800

    纠结，不知道为什么获取不到httpsession

--- src/main/java/com/wlh/develop/config/WebSocketConfig.java
+++ src/main/java/com/wlh/develop/config/WebSocketConfig.java
@@ -5,13 +5,20 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/systeminfo").withSockJS();
+        registry.addEndpoint("/systeminfo")
+                .setAllowedOrigins("/*").
+                // 引入HttpSessionHandshakeInterceptor,将httpSession复制到websocketSession
+                addInterceptors(new HttpSessionHandshakeInterceptor()).
+                withSockJS();
     }
 
     @Override
--- src/main/java/com/wlh/develop/controller/WatchController.java
+++ src/main/java/com/wlh/develop/controller/WatchController.java
@@ -2,15 +2,33 @@ package com.wlh.develop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.annotation.SendToUser;
-import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-@Controller
+@RestController
 public class WatchController {
 
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+
+    @Autowired
+    public WatchController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
     @MessageMapping("/cpuUseRate")
-    @SendToUser("/cpuinfo/cpuUseRate")
-    public String cpuUseRate(){
-       return null;
+    //@SendToUser("/cpuinfo/cpuUseRate")
+    //@Scheduled
+    public void cpuUseRate(SimpMessageHeaderAccessor headerAccessor) {
+        String sessionId =
+                (String)headerAccessor.getSessionAttributes().get(HttpSessionHandshakeInterceptor.HTTP_SESSION_ID_ATTR_NAME);
+        String sessionId2 = headerAccessor.getSessionId();
+        System.out.println(sessionId);
+        System.out.println(sessionId2);
+        //cpuCore.sendCpuUseRate(SpringContextUtils);
+        simpMessagingTemplate.convertAndSendToUser(sessionId2,"/cpuinfo/cpuUseRate",sessionId2);
     }
 }
