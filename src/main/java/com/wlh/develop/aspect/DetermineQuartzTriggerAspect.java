918809436baf20d63ef37e92028a15b18bb16124
weldon <weldon502@163.com>
2021-07-25 22:53:49 +0800

    注释DetermineQuartzTriggerAspect的代码

--- src/main/java/com/wlh/develop/aspect/DetermineQuartzTriggerAspect.java
+++ src/main/java/com/wlh/develop/aspect/DetermineQuartzTriggerAspect.java
@@ -17,36 +17,36 @@ import java.util.List;
 @Component
 @Aspect
 public class DetermineQuartzTriggerAspect {
-    private static final String SUFFX = "Trigger";
-
-    @Pointcut("@annotation(org.springframework.messaging.handler.annotation.MessageMapping)")
-    public void messageMappingAnnotation(){}
-
-    @Around("messageMappingAnnotation()")
-    public void around(ProceedingJoinPoint jp) throws Throwable{
-        String methodName = jp.getSignature().getName();
-        Class<?>[] parameterTypes = ((MethodSignature)jp.getSignature()).getParameterTypes();
-        Class<?> targetClass = jp.getTarget().getClass();
-        Method method = targetClass.getMethod(methodName,parameterTypes);
-        MessageMapping messageMapping = method.getDeclaredAnnotation(MessageMapping.class);
-        String[] values = messageMapping.value();
-
-        List<Object> args = new ArrayList<>();
-       if(jp.getArgs()!=null && jp.getArgs().length > 0){
-           for (int i = 0; i < jp.getArgs().length; i++) {
-               args.add(jp.getArgs()[i]);
-           }
-       }
-
-        if(values!=null && values.length > 0){
-            for(int i=0;i<values.length;i++){
-                args.add(values[i].substring(values[i].lastIndexOf("/")+1)+SUFFX);
-            }
-        }
-
-        jp.proceed();
-        //jp.proceed(args.toArray(new Object[args.size()]));
-    }
+//    private static final String SUFFX = "Trigger";
+//
+//    @Pointcut("@annotation(org.springframework.messaging.handler.annotation.MessageMapping)")
+//    public void messageMappingAnnotation(){}
+//
+//    @Around("messageMappingAnnotation()")
+//    public void around(ProceedingJoinPoint jp) throws Throwable{
+//        String methodName = jp.getSignature().getName();
+//        Class<?>[] parameterTypes = ((MethodSignature)jp.getSignature()).getParameterTypes();
+//        Class<?> targetClass = jp.getTarget().getClass();
+//        Method method = targetClass.getMethod(methodName,parameterTypes);
+//        MessageMapping messageMapping = method.getDeclaredAnnotation(MessageMapping.class);
+//        String[] values = messageMapping.value();
+//
+//        List<Object> args = new ArrayList<>();
+//       if(jp.getArgs()!=null && jp.getArgs().length > 0){
+//           for (int i = 0; i < jp.getArgs().length; i++) {
+//               args.add(jp.getArgs()[i]);
+//           }
+//       }
+//
+//        if(values!=null && values.length > 0){
+//            for(int i=0;i<values.length;i++){
+//                args.add(values[i].substring(values[i].lastIndexOf("/")+1)+SUFFX);
+//            }
+//        }
+//
+//        jp.proceed();
+//        //jp.proceed(args.toArray(new Object[args.size()]));
+//    }
 
 
 }
